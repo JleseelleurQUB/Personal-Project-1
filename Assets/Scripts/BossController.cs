3afebc709cb4feeb58253a098a1d9f1c3617c288
JleseelleurQUB <josephleseelleur@gmail.com>
2023-10-30 18:41:23 +0000

    Update 7
    - made start to boss AI, can calculate direction of player and moves in that direction
    
    - console log if boss collider connects with player

--- Assets/Scripts/BossController.cs
+++ Assets/Scripts/BossController.cs
@@ -2,8 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class BossController : MonoBehaviour
 {
+    private Vector3 playerDirection;
+    public GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,14 @@ public class BossController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // finds player direction and moves towards them, needs adjustment to not include vertical movement or it sinks into the floor
+        playerDirection = player.transform.position - transform.position;
+        transform.Translate(playerDirection.normalized * Time.deltaTime * 4);
+    }
+
+    // console tells player if they have been hit
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Player hit");
     }
 }
